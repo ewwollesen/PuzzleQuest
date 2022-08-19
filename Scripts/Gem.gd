e03e0886741eb7f7e2f958fa09da8159ec3e89a0
Wayne Wollesen <50209602+ewwollesen@users.noreply.github.com>
2022-08-19 10:31:59 -0500

    resetting

---
+++ Scripts/Gem.gd
@@ -0,0 +1,23 @@
+extends Node2D
+
+export (String) var type
+
+var matched: bool = false
+
+onready var moveTween: = $MoveTween
+onready var sprite: = $Sprite
+
+func move(target: Vector2) -> void:
+	moveTween.interpolate_property(self, "position", position, target, .3,
+			Tween.TRANS_LINEAR, Tween.EASE_OUT)
+	moveTween.start()
+
+
+func collapse(target: Vector2) -> void:
+	moveTween.interpolate_property(self, "position", position, target, .3,
+			Tween.TRANS_ELASTIC, Tween.EASE_OUT)
+	moveTween.start()
+
+	
+func dim():
+	sprite.modulate.a = .5
